20a4da15ffdcfc4f9ff8b288d8aab91923ba4cfd
ILOBA ISRAEL <israeliloba@gmail.com>
2019-07-05 16:26:31 +0100

    Product and Category Models and controllers

---
+++ database/migrations/2019_07_05_145918_create_products_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('category_id');
+            $table->string('name')->unique();
+            $table->string('slug')->unique();
+            $table->text('short_desc');
+            $table->text('long_desc');
+            $table->float('price');
+            $table->string('image');
+            $table->boolean('status')->default(1);
+
+            $table->index('category_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
